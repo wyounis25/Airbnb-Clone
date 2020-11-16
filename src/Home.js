df0dd268788438b955a5030bb46968a077d9e3b7
wyounis25 <github email address>
2020-11-15 21:15:36 -0600

    header and banner style

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Home.css"
+import Banner from './Banner'
+import './Banner.css'
 
 function Home() {
     return (
         <div>
             <h1>HOME</h1>
+            <Banner/>
         </div>
     )
 }
